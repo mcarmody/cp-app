853832a62d5b01bd09c2fcafe89663674496fa96
mcarmody <mcarmody2013@gmail.com>
2019-08-22 13:09:49 -0400

    header first pass

---
+++ src/header.js
@@ -0,0 +1,20 @@
+// create.component.js
+
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className = "header-container">
+	          <div className = "header-copy-container">
+	            <div className = "schedule-title">Schedule Service</div>
+	            <div className = "store-contact-info">
+	              <div className = "store-name">Gregory's Appliance Repairs, LLC.</div>
+	              <div className = "store-phone">555-555-9999</div>
+	            </div>
+	            <div className = "language-select">Español  Français</div>
+	          </div>
+	      </div>
+        )
+    }
+}
