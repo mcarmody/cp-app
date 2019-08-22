d8620c9a1c2674a87102d45431a26afb5e30a21a
mcarmody <mcarmody2013@gmail.com>
2019-08-22 13:43:37 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Header from './header.js';
 import ProgressTracker from './progress-tracker.js';
+import Footer from './footer.js';
 import './header.css';
 import './progress-tracker.css';
+import  './footer.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <ProgressTracker/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className = "footer-container">
+				<div className = "footer-disclaimer">
+					Asurion 1994 - 2019. All rights reserved  |  Terms of Use  |  Privacy Policy
+				</div>
+	      	</div>
+        )
+    }
+}
